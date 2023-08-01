d94399cecc52e44298ce6362195dbbb11e7db95c
wladimirrojas <rajimiru@gmail.com>
2023-08-01 01:06:06 -0400

    added all the implementations to save a picture as file from listing

--- src/main/java/com/example/springbootwebflux/app/controller/ProductController.java
+++ src/main/java/com/example/springbootwebflux/app/controller/ProductController.java
@@ -7,6 +7,12 @@ import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,14 +20,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import org.thymeleaf.spring6.context.webflux.ReactiveDataDriverContextVariable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @SessionAttributes("product")
 @Controller
@@ -30,11 +42,41 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
+    @Value("${config.pictures.path}")
+    private String path;
+
     private static final Logger log = LoggerFactory.getLogger(ProductController.class);
 
     @ModelAttribute("categories")
     public Flux<Category> categories() {
         return service.findAllCategory();
+    }
+
+    @GetMapping("/look/{id}")
+    public Mono<String> look(Model model, @PathVariable String id) {
+        return service.findById(id)
+                .doOnNext(p -> {
+                    model.addAttribute("product", p);
+                    model.addAttribute("title", "Product detail");
+                }).switchIfEmpty(Mono.just(new Product()))
+                .flatMap(p -> {
+                    if (p.getId() == null) return Mono.error(new InterruptedException("Product does not exist"));
+                    return Mono.just(p);
+                }).then(Mono.just("look"))
+                .onErrorResume(ex -> Mono.just("redirect:/listing?error=product+does+not+exist"));
+    }
+
+    @GetMapping("/pictures/img/{pictureName:.+}")
+    public Mono<ResponseEntity<Resource>> lookPicture(@PathVariable String pictureName) throws MalformedURLException {
+        Path picturePath = Paths.get(path).resolve(pictureName).toAbsolutePath();
+
+        Resource picture = new UrlResource(picturePath.toUri());
+
+        return Mono.just(
+                ResponseEntity.ok()
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + picture.getFilename() + "\"")
+                        .body(picture)
+        );
     }
 
     @GetMapping("/listing-chunked")
@@ -131,7 +173,7 @@ public class ProductController {
     }
 
     @PostMapping("/form")
-    public Mono<String> save(@Valid Product product, BindingResult result, Model model, SessionStatus status) {
+    public Mono<String> save(@Valid Product product, BindingResult result, Model model, @RequestPart FilePart file, SessionStatus status) {
 
         if (result.hasErrors()) {
             model.addAttribute("title", "Error in product form");
@@ -141,13 +183,28 @@ public class ProductController {
 
         status.setComplete();
 
-        if (product.getCreatedAt() == null) {
-            product.setCreatedAt(LocalDateTime.now());
-        }
 
-        return service.save(product).doOnNext(p -> {
+
+        Mono<Category> category = service.findCategoryById(product.getCategory().getId());
+
+        return category.flatMap(c -> {
+            if (product.getCreatedAt() == null) product.setCreatedAt(LocalDateTime.now());
+            if (!file.filename().isEmpty()) product.setPicture(UUID.randomUUID().toString() + "-" + file.filename()
+                    .replace(" ", "")
+                    .replace(":", "")
+                    .replace("\\", ""));
+
+            product.setCategory(c);
+            return service.save(product);
+        }).doOnNext(p -> {
             log.info("Product saved: " + p.getName() + " Id: " + p.getId());
-        }).thenReturn("redirect:/listing?success=product+saved+successfully");
+            log.info("With Category: " + p.getCategory().getName() + " Id: " + p.getCategory().getId());
+
+        }).flatMap(p -> {
+                    if (!file.filename().isEmpty()) return file.transferTo(new File(path + p.getPicture()));
+                    return Mono.empty();
+                })
+                .thenReturn("redirect:/listing?success=product+saved+successfully");
     }
 
     @GetMapping("/delete/{id}")
